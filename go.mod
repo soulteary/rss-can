bd04ba91722efb123432ae3aa2eb721bce9776f1
soulteary <soulteary@gmail.com>
2022-12-13 12:30:44 +0800

    chore: add simple charset fn to decode remote body

--- go.mod
+++ go.mod
@@ -4,13 +4,12 @@ go 1.19
 
 require (
 	github.com/PuerkitoBio/goquery v1.8.0
+	golang.org/x/net v0.2.0
+	golang.org/x/text v0.4.0
 	rogchap.com/v8go v0.7.0
 )
 
-require (
-	github.com/andybalholm/cascadia v1.3.1 // indirect
-	golang.org/x/net v0.2.0 // indirect
-)
+require github.com/andybalholm/cascadia v1.3.1 // indirect
 
 replace github.com/PuerkitoBio/goquery => ./pkg/PuerkitoBio/goquery
 
---
+++ internal/charset/charset.go
@@ -0,0 +1,36 @@
+package utils
+
+import (
+	"bufio"
+	"io"
+
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding/htmlindex"
+)
+
+// According to the returned content, automatically determine the content encoding format
+func DetectContentCharset(body io.Reader) string {
+	r := bufio.NewReader(body)
+	if data, err := r.Peek(1024); err == nil {
+		if _, name, ok := charset.DetermineEncoding(data, ""); ok {
+			return name
+		}
+	}
+	return "utf-8"
+}
+
+// decodeHTMLBody returns an decoding reader of the html Body for the specified `charset`
+// If `charset` is empty, decodeHTMLBody tries to guess the encoding from the content
+func DecodeHTMLBody(body io.Reader, charset string) (io.Reader, error) {
+	if charset == "" {
+		charset = DetectContentCharset(body)
+	}
+	e, err := htmlindex.Get(charset)
+	if err != nil {
+		return nil, err
+	}
+	if name, _ := htmlindex.Name(e); name != "utf-8" {
+		body = e.NewDecoder().Reader(body)
+	}
+	return body, nil
+}

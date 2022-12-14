2cfc1b3b2d70d79470fad6b099d0d1b908807a8b
soulteary <soulteary@gmail.com>
2022-12-14 16:54:36 +0800

    chore: split ssr parser

---
+++ internal/parser/ssr.go
@@ -0,0 +1,41 @@
+package parser
+
+import (
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/soulteary/RSS-Can/internal/define"
+	"github.com/soulteary/RSS-Can/internal/network"
+)
+
+func GetWebsiteDataWithConfig(config define.JavaScriptConfig) (result define.BodyParsed) {
+	doc := network.GetRemoteDocument("https://36kr.com/", "utf-8")
+	if doc.Body == "" {
+		return result
+	}
+
+	return ParsePageByGoQuery(doc, func(document *goquery.Document) []define.InfoItem {
+		var items []define.InfoItem
+		document.Find(config.ListContainer).Each(func(i int, s *goquery.Selection) {
+			var item define.InfoItem
+
+			title := strings.TrimSpace(s.Find(config.Title).Text())
+			author := strings.TrimSpace(s.Find(config.Author).Text())
+			time := strings.TrimSpace(s.Find(config.DateTime).Text())
+			category := strings.TrimSpace(s.Find(config.Category).Text())
+			description := strings.TrimSpace(s.Find(config.Description).Text())
+
+			href, _ := s.Find(config.Link).Attr("href")
+			link := strings.TrimSpace(href)
+
+			item.Title = title
+			item.Author = author
+			item.Date = time
+			item.Category = category
+			item.Description = description
+			item.Link = link
+			items = append(items, item)
+		})
+		return items
+	})
+}
--- main.go
+++ main.go
@@ -7,47 +7,13 @@ import (
 	"strings"
 	"time"
 
-	"github.com/PuerkitoBio/goquery"
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/feeds"
 
 	"github.com/soulteary/RSS-Can/internal/define"
 	"github.com/soulteary/RSS-Can/internal/javascript"
-	"github.com/soulteary/RSS-Can/internal/network"
 	"github.com/soulteary/RSS-Can/internal/parser"
 )
-
-func getWebsiteDataWithConfig(config define.JavaScriptConfig) (result define.BodyParsed) {
-	doc := network.GetRemoteDocument("https://36kr.com/", "utf-8")
-	if doc.Body == "" {
-		return result
-	}
-
-	return parser.ParsePageByGoQuery(doc, func(document *goquery.Document) []define.InfoItem {
-		var items []define.InfoItem
-		document.Find(config.ListContainer).Each(func(i int, s *goquery.Selection) {
-			var item define.InfoItem
-
-			title := strings.TrimSpace(s.Find(config.Title).Text())
-			author := strings.TrimSpace(s.Find(config.Author).Text())
-			time := strings.TrimSpace(s.Find(config.DateTime).Text())
-			category := strings.TrimSpace(s.Find(config.Category).Text())
-			description := strings.TrimSpace(s.Find(config.Description).Text())
-
-			href, _ := s.Find(config.Link).Attr("href")
-			link := strings.TrimSpace(href)
-
-			item.Title = title
-			item.Author = author
-			item.Date = time
-			item.Category = category
-			item.Description = description
-			item.Link = link
-			items = append(items, item)
-		})
-		return items
-	})
-}
 
 func generateFeeds(data define.BodyParsed, rssType string) string {
 	now := time.Now()
@@ -107,7 +73,7 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-	data := getWebsiteDataWithConfig(config)
+	data := parser.GetWebsiteDataWithConfig(config)
 
 	type RSSType struct {
 		Type string `uri:"type" binding:"required"`

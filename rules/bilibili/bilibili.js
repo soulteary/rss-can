109451c65547444a2cd7f08245ff0721cd588d10
soulteary <soulteary@gmail.com>
2022-12-15 22:53:05 +0800

    feat: add bilibili rules

---
+++ rules/bilibili/bilibili.js
@@ -0,0 +1,12 @@
+const config = {
+    URL: "https://www.bilibili.com/read/home",
+    Mode: "csr",
+    Title: ".article-title",
+    Author: ".article-info-bar .up-content .nick-name",
+    Category: ".article-info-bar .category",
+    // DateTime: "(sub page)#app .title-container .article-read-info .publish-text",
+    Description: ".article-desc",
+    Link: ".article-title-holder",
+  };
+  potted.SetConfig(config).SetScope(".article-list .article-item");
+  

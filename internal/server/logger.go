196dba3463834bbbeabca7ad858a682dd6140745
soulteary <soulteary@gmail.com>
2022-12-20 00:52:35 +0800

    feat: update gin logger

---
+++ internal/server/logger.go
@@ -0,0 +1,90 @@
+package server
+
+// modify file: https://github.com/toorop/gin-logrus/blob/master/logger.go
+
+import (
+	"fmt"
+	"math"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+)
+
+// 2016-09-27 09:38:21.541541811 +0200 CEST
+// 127.0.0.1 - frank [10/Oct/2000:13:55:36 -0700]
+// "GET /apache_pb.gif HTTP/1.0" 200 2326
+// "http://www.example.com/start.html"
+// "Mozilla/4.08 [en] (Win98; I ;Nav)"
+
+var timeFormat = "02/Jan/2006:15:04:05 -0700"
+
+func Logger(logger *zap.SugaredLogger, notLogged ...string) gin.HandlerFunc {
+	hostname, err := os.Hostname()
+	if err != nil {
+		hostname = "unknow"
+	}
+
+	var skip map[string]struct{}
+
+	if length := len(notLogged); length > 0 {
+		skip = make(map[string]struct{}, length)
+
+		for _, p := range notLogged {
+			skip[p] = struct{}{}
+		}
+	}
+
+	return func(c *gin.Context) {
+		// other handler can change c.Path so:
+		path := inputSanitized(c.Request.URL.Path)
+		start := time.Now()
+		c.Next()
+		stop := time.Since(start)
+		latency := int(math.Ceil(float64(stop.Nanoseconds()) / 1000000.0))
+		statusCode := c.Writer.Status()
+		clientIP := inputSanitized(c.ClientIP())
+		clientUserAgent := inputSanitized(c.Request.UserAgent())
+		referer := inputSanitized(c.Request.Referer())
+		dataLength := c.Writer.Size()
+		if dataLength < 0 {
+			dataLength = 0
+		}
+
+		if _, ok := skip[path]; ok {
+			return
+		}
+
+		entry := logger.With(
+			"hostname", hostname,
+			"statusCode", statusCode,
+			"latency", latency, // time to process
+			"clientIP", clientIP,
+			"method", c.Request.Method,
+			"path", path,
+			"referer", referer,
+			"dataLength", dataLength,
+			"userAgent", clientUserAgent,
+		)
+
+		if len(c.Errors) > 0 {
+			entry.Error(c.Errors.ByType(gin.ErrorTypePrivate).String())
+		} else {
+			msg := fmt.Sprintf("%s - %s [%s] \"%s %s\" %d %d \"%s\" \"%s\" (%dms)", clientIP, hostname, time.Now().Format(timeFormat), c.Request.Method, path, statusCode, dataLength, referer, clientUserAgent, latency)
+			if statusCode >= http.StatusInternalServerError {
+				entry.Error(msg)
+			} else if statusCode >= http.StatusBadRequest {
+				entry.Warn(msg)
+			} else {
+				entry.Info(msg)
+			}
+		}
+	}
+}
+
+func inputSanitized(input string) string {
+	return strings.Replace(strings.Replace(input, "\n", "", -1), "\r", "", -1)
+}
--- internal/server/server.go
+++ internal/server/server.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/soulteary/RSS-Can/internal/define"
+	"github.com/soulteary/RSS-Can/internal/logger"
 	"github.com/soulteary/RSS-Can/internal/rule"
 )
 
@@ -12,6 +13,8 @@ func ServAPI() {
 	rule.InitRules()
 
 	route := gin.Default()
+
+	route.Use(Logger(logger.Instance), gin.Recovery())
 	route.GET("/:id/:type/", apiRSS())
 	route.GET("/config/:type/:value/", apiConfig())
 	route.GET("/", welcomePage())

5fbe8283d66875e97a13b477de0fe852128ade51
soulteary <soulteary@gmail.com>
2022-12-19 21:52:33 +0800

    chore: split server const

---
+++ internal/server/welcome.go
@@ -0,0 +1,17 @@
+package server
+
+const welcomePageForTest = `<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>RSS Feed Discovery.</title>
+	<link rel="alternate" type="application/rss+xml" title="RSS 2.0 Feed" href="http://localhost:8080/36kr/rss">
+	<link rel="alternate" type="application/atom+xml" title="RSS Atom Feed" href="http://localhost:8080/36kr/atom">
+	<link rel="alternate" type="application/rss+json" title="RSS JSON Feed" href="http://localhost:8080/36kr/json">
+</head>
+<body>
+	RSS Feed Discovery.
+</body>
+</html>`

00087504d7a5df3675d17cb688b1517e1afa57e2
soulteary <soulteary@gmail.com>
2022-12-21 21:29:11 +0800

    test: fix sandbox test

--- internal/javascript/sandbox_test.go
+++ internal/javascript/sandbox_test.go
@@ -1,21 +1,29 @@
-package javascript
+package javascript_test
 
 import (
+	"fmt"
 	"testing"
 	"time"
 
 	"github.com/soulteary/RSS-Can/internal/define"
 	"github.com/soulteary/RSS-Can/internal/fn"
+	"github.com/soulteary/RSS-Can/internal/javascript"
+	"github.com/soulteary/RSS-Can/internal/logger"
 )
+
+func init() {
+	logger.Initialize()
+}
 
 func TestRunCode(t *testing.T) {
 	// test forever loops
 	start := time.Now()
-	_, err := RunCode(`while(1){console.log(1)}`, "")
+	_, err := javascript.RunCode(`while(1){console.log(1)}`, "")
 	if err == nil {
 		t.Fatalf("Programs executed without aborting timeouts")
 	}
 	duration := time.Since(start)
+	fmt.Println(duration)
 	if duration > (fn.I2T(define.DEFAULT_JS_EXECUTE_TIMEOUT) * time.Millisecond * 100) {
 		t.Fatalf("Code execution takes longer than expected")
 	}

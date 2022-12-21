6a3cbdd5c5a6b0a94f645414a14612c72670312d
soulteary <soulteary@gmail.com>
2022-12-22 00:15:44 +0800

    chore: update build flags

--- internal/define/global.go
+++ internal/define/global.go
@@ -1,7 +1,7 @@
 package define
 
 var (
-	DEBUG_MODE      = true
+	DEBUG_MODE      = false
 	DEBUG_LEVEL     = "info"
 	USER_AGENT      = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
 	REQUEST_TIMEOUT = 5 // seconds, fetch remote data timeout

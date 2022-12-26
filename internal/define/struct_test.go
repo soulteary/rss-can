143f047f59f5aff04e9e2ca9e2625247477197a0
soulteary <soulteary@gmail.com>
2022-12-26 16:52:28 +0800

    test: full coverage for internal/define

---
+++ internal/define/struct_test.go
@@ -0,0 +1,40 @@
+package define_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/soulteary/RSS-Can/internal/define"
+	"github.com/soulteary/RSS-Can/internal/fn"
+)
+
+func TestMixupRemoteBodySanitized(t *testing.T) {
+	code := define.ERROR_CODE_NULL
+	status := "dummy status"
+	date := time.Now()
+	body := "hello world"
+
+	object := define.MixupRemoteBodySanitized(code, status, date, body)
+
+	if !(object.Code == code && object.Status == status && object.Date == date && object.Body == body) {
+		t.Fatal("MixupRemoteBodySanitized failed")
+	}
+}
+
+func TestMixupBodyParsed(t *testing.T) {
+	code := define.ERROR_CODE_NULL
+	status := "dummy status"
+	date := time.Now()
+	var item []define.InfoItem
+
+	object := define.MixupBodyParsed(code, status, date, item)
+	if !(object.Code == code && object.Status == status && object.Date == date) {
+		t.Fatal("MixupBodyParsed failed")
+	}
+
+	f1, _ := fn.JSONStringify(object.Body)
+	f2, _ := fn.JSONStringify(item)
+	if f1 != f2 {
+		t.Fatal("MixupBodyParsed failed")
+	}
+}
